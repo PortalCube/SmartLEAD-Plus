24357d94f026d94746b3fa7a1942cb430dcd0498
PortalCube <35104213+PortalCube@users.noreply.github.com>
2020-09-07 20:24:39 +0900

    Create main.js

---
+++ main.js
@@ -0,0 +1,13 @@
+// ==UserScript==
+// @name         SmartLEAD+
+// @namespace    https://github.com/PortalCube/
+// @version      0.1.0
+// @description  한림대학교의 LMS, SmartLEAD를 더욱 Smart하게 만들어주는 확장스크립트입니다.
+// @author       PortalCube@hallym
+// @match        https://smartlead.hallym.ac.kr/*
+// @grant        none
+// ==/UserScript==
+
+'use strict'
+
+// TODO
