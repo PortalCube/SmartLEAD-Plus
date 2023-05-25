cf475cd8e048fd98a81f3d1bb882e2827aae3a3b
PortalCube <35104213+PortalCube@users.noreply.github.com>
2023-05-25 23:04:15 +0900

    feat: todo 페이지 구현

--- src/script/dom-manager.ts
+++ src/script/dom-manager.ts
@@ -2,16 +2,133 @@
 
 import LandingPage from "../html/landing/main.html?raw";
 import LandingCourse from "../html/landing/course.html?raw";
-import LandingTodo from "../html/landing/todo.html?raw";
+import LandingTodoContainer from "../html/landing/todo.html?raw";
+import LandingTodoItem from "../html/landing/todo-item.html?raw";
 import LandingWeekly from "../html/landing/weekly.html?raw";
 import CourseProgress from "../html/course/progress.html?raw";
 
+import dayjs from "dayjs";
+
 import { CourseManager } from "./course-manager";
+import { ActivityType, StringToNode } from "./util";
+
+const $ = document.querySelector.bind(document);
+const $$ = document.querySelectorAll.bind(document);
 
 export const DOMManager = {
-    BuildTodoListElement() {},
+    BuildLandingPage() {
+        const element = $<HTMLDivElement>(".progress_courses");
 
-    BuildLandingPageElement() {},
+        if (!element) {
+            console.error(`BuildLandingPageElement: 랜딩 페이지 Injection 실패`);
+            return;
+        }
 
-    BuildButtonGroup() {}
+        element.classList.add("smartlead-plus");
+
+        element.innerHTML = LandingPage;
+
+        // Summery Page Build
+
+        this.BuildTodoElement();
+    },
+
+    BuildTodoElement() {
+        const container = $<HTMLDivElement>(".smartlead-plus .landing-container");
+
+        if (!container) {
+            console.error("BuildTodoElement: 컨테이너 Element를 찾을 수 없습니다.");
+            return;
+        }
+
+        const todoContainer = StringToNode(LandingTodoContainer)[0] as HTMLDivElement;
+
+        container.appendChild(todoContainer);
+
+        const list = CourseManager.activitys.filter(
+            (item) =>
+                !item.complete &&
+                [
+                    ActivityType.Assignment,
+                    ActivityType.Video,
+                    ActivityType.Quiz,
+                    ActivityType.Zoom
+                ].includes(item.type) &&
+                item.date.end_date !== null &&
+                dayjs(item.date.end_date) > dayjs()
+        );
+
+        list.sort((a, b) => {
+            const date =
+                dayjs(a.date.end_date).valueOf() - dayjs(b.date.end_date).valueOf();
+            if (date === 0) {
+                const course = a.course_id - b.course_id;
+                if (course === 0) {
+                    return a.name.localeCompare(b.name);
+                } else {
+                    return course;
+                }
+            } else {
+                return date;
+            }
+        });
+
+        for (const item of list) {
+            let url = "",
+                icon = "";
+
+            switch (item.type) {
+                case ActivityType.Video:
+                    url = "/mod/vod/viewer.php?id=";
+                    icon = "/theme/image.php/coursemosv2/vod/1599440449/icon";
+                    break;
+                case ActivityType.Assignment:
+                    url = "/mod/assign/view.php?id=";
+                    icon = "/theme/image.php/coursemosv2/assign/1599440449/icon";
+                    break;
+                case ActivityType.Quiz:
+                    url = "/mod/quiz/view.php?id=";
+                    icon = "/theme/image.php/coursemosv2/quiz/1599440449/icon";
+                    break;
+                case ActivityType.Zoom:
+                    url = "/mod/zoom/view.php?id=";
+                    icon = "/theme/image.php/coursemosv2/zoom/1599440449/icon";
+                    break;
+            }
+
+            url += item.id.toString();
+
+            const activityName =
+                item.name.length > 15 ? item.name.slice(0, 15) + "..." : item.name;
+            const courseName =
+                CourseManager.courses.find((course) => course.id === item.course_id)
+                    ?.name ?? "";
+            const endDate = dayjs(item.date.end_date);
+            const timeLeft = endDate.fromNow(true) + " 뒤 마감";
+            const expireDate = "마감일: " + endDate.format("YYYY.MM.DD HH:mm");
+
+            let level = 0;
+            const timeLeftHour = Math.abs(dayjs().diff(endDate, "hour"));
+
+            if (timeLeftHour < 6) {
+                level = 4; // 6시간
+            } else if (timeLeftHour < 24) {
+                level = 3; // 1일
+            } else if (timeLeftHour < 24 * 5) {
+                level = 2; // 5일
+            } else if (timeLeftHour < 24 * 10) {
+                level = 1; // 10일
+            }
+
+            let itemElement = LandingTodoItem.replace("{URL}", url)
+                .replace("{ICON}", icon)
+                .replace("{TITLE}", activityName)
+                .replace("{SUBTITLE}", courseName)
+                .replace("{LEVEL}", level.toString())
+                .replace("{TIMELEFT}", timeLeft)
+                .replace("{EXPIRE}", expireDate);
+
+            todoContainer.innerHTML += itemElement;
+        }
+    }
 };
