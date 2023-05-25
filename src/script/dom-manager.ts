6245aa7962065ad8187fe158d286513da1c075ae
PortalCube <35104213+PortalCube@users.noreply.github.com>
2023-05-25 19:33:27 +0900

    fix: law -> raw typo fix

--- src/script/dom-manager.ts
+++ src/script/dom-manager.ts
@@ -1,10 +1,10 @@
 "use strict";
 
-import LandingPage from "../html/landing/main.html?law";
-import LandingCourse from "../html/landing/course.html?law";
-import LandingTodo from "../html/landing/todo.html?law";
-import LandingWeekly from "../html/landing/weekly.html?law";
-import CourseProgress from "../html/course/progress.html?law";
+import LandingPage from "../html/landing/main.html?raw";
+import LandingCourse from "../html/landing/course.html?raw";
+import LandingTodo from "../html/landing/todo.html?raw";
+import LandingWeekly from "../html/landing/weekly.html?raw";
+import CourseProgress from "../html/course/progress.html?raw";
 
 import { CourseManager } from "./course-manager";
 
