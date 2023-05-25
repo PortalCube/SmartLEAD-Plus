899bc4e966f8576ed3953b43bf2342b288a04aa0
PortalCube <35104213+PortalCube@users.noreply.github.com>
2023-05-25 19:20:05 +0900

    chore: html 파일 import 추가

--- src/script/dom-manager.ts
+++ src/script/dom-manager.ts
@@ -1,3 +1,13 @@
+"use strict";
+
+import LandingPage from "../html/landing/main.html?law";
+import LandingCourse from "../html/landing/course.html?law";
+import LandingTodo from "../html/landing/todo.html?law";
+import LandingWeekly from "../html/landing/weekly.html?law";
+import CourseProgress from "../html/course/progress.html?law";
+
+import { CourseManager } from "./course-manager";
+
 export const DOMManager = {
     BuildTodoListElement() {},
 
