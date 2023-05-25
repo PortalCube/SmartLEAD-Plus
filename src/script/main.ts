42d05f22a5bd42941736b9fac72c6207fef184e5
PortalCube <35104213+PortalCube@users.noreply.github.com>
2023-05-25 23:03:55 +0900

    feat: 진입점 코드 구현

--- src/script/main.ts
+++ src/script/main.ts
@@ -1,9 +1,50 @@
 "use strict";
-import { CourseManager } from "./course-manager";
+import dayjs from "dayjs";
+import relativeTime from "dayjs/plugin/relativeTime";
+import arraySupport from "dayjs/plugin/arraySupport";
+import objectSupport from "dayjs/plugin/objectSupport";
+import "dayjs/locale/ko";
 
-(async () => {
-    await CourseManager.Load();
-    console.log(CourseManager.courses);
-    const activitys = CourseManager.activitys;
-    console.log(activitys);
+import "./style.scss";
+
+import { CourseManager } from "./course-manager";
+import { DOMManager } from "./dom-manager";
+
+export const ExtensionManager = {
+    async Init() {
+        // 데이터 불러오기
+        await CourseManager.LoadData();
+
+        console.log(CourseManager.courses);
+
+        // 경로에 따라서 페이지 구성하기
+        switch (location.pathname) {
+            case "/":
+                // 메인 페이지
+                await this.InitLandingPage();
+                break;
+            case "/course/view.php":
+                // 강좌 메인 페이지
+                await this.InitCoursePage();
+                break;
+        }
+    },
+
+    async InitLandingPage() {
+        DOMManager.BuildLandingPage();
+    },
+
+    async InitCoursePage() {},
+
+    async InitVideoPage() {}
+};
+
+// 패키지 세팅
+(() => {
+    dayjs.extend(relativeTime);
+    dayjs.extend(arraySupport);
+    dayjs.extend(objectSupport);
+    dayjs.locale("ko");
+
+    ExtensionManager.Init();
 })();
